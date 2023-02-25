59410da76c285eb2335eed84daf20f9c88e59297
Ataugur Karatas <ataugurkaratas@gmail.com>
2023-02-25 13:05:59 +0300

    Added Admin classes and interface

---
+++ src/main/java/com/example/credit/controller/AdminController.java
@@ -0,0 +1,49 @@
+package com.example.credit.controller;
+
+import com.example.credit.dto.request.AdminInfoUpdateRequest;
+import com.example.credit.dto.request.AuthAdminRegister;
+import com.example.credit.dto.request.AuthEmployeeRegister;
+import com.example.credit.dto.response.AdminResponse;
+import com.example.credit.service.impl.AdminServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+    private final AdminServiceImpl adminService;
+
+    public AdminController(AdminServiceImpl adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/employee/save")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<String> saveEmployee(@Valid @RequestBody AuthEmployeeRegister authEmployeeRegister){
+        adminService.saveEmployee(authEmployeeRegister);
+        return new ResponseEntity<>("User Created", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/save")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<String> saveAdmin(@Valid @RequestBody AuthAdminRegister authAdminRegister){
+        adminService.saveAdmin(authAdminRegister);
+        return new ResponseEntity<>("User Created", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/admin-info-update")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<String> updateAdminInfo(@Valid @RequestBody AdminInfoUpdateRequest adminInfoUpdateRequest){
+        adminService.updateAdminInfo(adminInfoUpdateRequest);
+        return new ResponseEntity<>("Admin Info Updated", HttpStatus.OK);
+    }
+
+    @GetMapping("/{authId}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public AdminResponse getAdmin(@PathVariable String authId){
+        return adminService.getAdminByAuthId(authId);
+    }
+}
---
+++ src/test/java/com/example/credit/service/impl/AdminServiceImplTest.java
@@ -0,0 +1,161 @@
+package com.example.credit.service.impl;
+
+import com.example.credit.dto.CreditRatingDto;
+import com.example.credit.dto.request.AdminInfoUpdateRequest;
+import com.example.credit.dto.request.AuthAdminRegister;
+import com.example.credit.dto.request.AuthEmployeeRegister;
+import com.example.credit.exception.NotFoundException;
+import com.example.credit.model.Admin;
+import com.example.credit.model.Auth;
+import com.example.credit.repository.AdminRepository;
+import com.example.credit.util.ImageProcessing;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AdminServiceImplTest {
+
+    @InjectMocks
+    private AdminServiceImpl adminService;
+
+    @Mock
+    private AdminRepository adminRepository;
+
+    @Mock
+    private EmployeeServiceImpl employeeService;
+
+    @Mock
+    private AuthServiceImpl authService;
+
+    @Mock
+    private CreditRatingServiceImpl creditRatingService;
+
+    @Mock
+    private ImageProcessing imageConvert;
+
+    @Test
+    void ShouldSaveAdminWhenInputsAreCorrect() {
+        AuthAdminRegister authAdminRegister = new AuthAdminRegister("12345678901"
+                , "Ataugur"
+                , "Karatas"
+                , "ataugurkaratas@gmail.com"
+                , LocalDate.of(1998, 6, 4)
+                , "905405404040");
+
+
+        Auth auth = new Auth("id"
+                , "12345678901"
+                , "ataugurkaratas@gmail.com"
+                , "password"
+                , "ROLE_ADMIN"
+                , true
+                , true);
+
+        when(authService.addUserAdmin(authAdminRegister)).thenReturn(auth);
+        adminService.saveAdmin(authAdminRegister);
+
+        Admin admin = new Admin("Ataugur"
+                , "Karatas"
+                , "905405404040"
+                , "profile.jpeg"
+                , LocalDate.of(1998, 6, 4)
+                , auth);
+
+        verify(adminRepository).save(admin);
+    }
+
+    @Test
+    void ShouldSaveEmployee(){
+        AuthEmployeeRegister authEmployeeRegister =
+                new AuthEmployeeRegister("1111111111"
+                ,"Ataugur"
+                ,"Karatas"
+                ,"ataugurkaratas@gmail.com"
+                ,LocalDate.of(1998,6,4)
+                ,"905405404040");
+        adminService.saveEmployee(authEmployeeRegister);
+    }
+
+    @Test
+    void ShouldSaveCurrentCreditRating(){
+        CreditRatingDto creditRatingDto = new CreditRatingDto(25,25,25,25,250);
+        adminService.saveCurrentCreditRating(creditRatingDto);
+    }
+
+    @Test
+    void ShouldUpdateAdminInfoWhenInputsAreCorrect() {
+        AdminInfoUpdateRequest adminInfoUpdateRequest = new AdminInfoUpdateRequest("id"
+                , "Ataugur"
+                , "Karatas"
+                , "905425424242"
+                , LocalDate.of(1998, 6, 4)
+                , "profile.jpeg");
+
+        Auth auth = new Auth();
+
+        Admin admin = new Admin("Ataugur"
+                , "Karatas"
+                , "905405404040"
+                , "profile.jpeg"
+                , LocalDate.of(1998, 6, 4)
+                , auth);
+
+        when(adminRepository.findById(adminInfoUpdateRequest.adminId())).thenReturn(Optional.of(admin));
+        adminService.updateAdminInfo(adminInfoUpdateRequest);
+
+        verify(adminRepository).save(admin);
+    }
+
+    @Test
+    void ShouldReturnAdminResponseWhenAuthId(){
+        String authId = "id";
+
+        Auth auth = new Auth();
+        Admin admin = new Admin("Ataugur"
+                , "Karatas"
+                , "905405404040"
+                , "profile.jpeg"
+                , LocalDate.of(1998, 6, 4)
+                , auth);
+
+        when(adminRepository.getAdminByAuthId(authId)).thenReturn(Optional.of(admin));
+
+        adminService.getAdminByAuthId(authId);
+    }
+
+    @Test
+    void ShouldThrowExceptionWhenUpdateAdminInfo(){
+        AdminInfoUpdateRequest request = new AdminInfoUpdateRequest("id"
+                , "Ataugur"
+                , "Karatas"
+                , "905425424242"
+                , LocalDate.of(1998, 6, 4)
+                , "profile.jpeg");
+
+        NotFoundException exception = assertThrows(NotFoundException.class, () ->
+                adminService.updateAdminInfo(request));
+
+        assertEquals("Admin Not Found", exception.getMessage());
+    }
+
+    @Test
+    void ShouldThrowExceptionWhenGetAdminByAuthId(){
+        String authId = "id";
+
+        NotFoundException exception = assertThrows(NotFoundException.class, () ->
+                adminService.getAdminByAuthId(authId));
+
+        assertEquals("Admin Not Found", exception.getMessage());
+    }
+}
